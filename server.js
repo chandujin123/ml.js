5dbcd664fadbe1317685d00b5107dd6256d09ede
raghavendra <raghavendra.bobbili@gmail.com>
2016-02-22 20:50:55 +0530

    delete qfuncanality is added

--- server.js
+++ server.js
@@ -12,16 +12,34 @@ conn.get('/todos',function(req,res){
 conn.post('/todos',function(req,res){
 	var bd = req.body;
 	var picked = _.pick(bd,'description','completed');
-	//consolelog(!_.isBoolean(bd.completed)+'  '+)
+	console.log(!_.isBoolean(bd.completed)+'  ds');
 	if(!_.isBoolean(bd.completed) || !_.isString(bd.description))
 	{
 		return res.status(404).send();
 	}
 	bd.id=idi;
+	picked.id=idi;
 	idi=idi+1;
 	todos.push(picked);
+
 	console.log('in post'+JSON.stringify(todos));
 	res.json(bd);
+});
+conn.delete('/todos/:id' , function(req,res){
+	var rid=parseInt(req.params.id);
+	console.log('this in delete'+rid);
+	var mached=_.findWhere(todos,{id:rid});
+	if(!mached)
+	{
+		res.status(404).json({"error":"this is error msg"});
+		
+	}
+	else
+	{
+		todos = _.without(todos,mached);
+		res.json(mached);
+	}
+	
 });
 conn.get('/todos/:id',function(req,res){
 	//colsole.log("in id get request")
@@ -43,7 +61,6 @@ conn.get('/todos/:id',function(req,res){
 conn.get('/',function(req,res){
 	res.send('TO DO API ROOT');
 });
-
 
 conn.listen(port,function(){
 		console.log('listningt to 3000  '+port);
